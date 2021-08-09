52ad39a255005c0e46626e5346e147ba89657f9d
Vladislav Belousov <v.belousov@labmedia.su>
2021-08-09 18:28:52 +0300

    создание основы для бота

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const loadJsonFile = require('load-json-file');
+const fs = require('fs');
 const vacancies = require('./vacancies.json');
 const api_key = '1563de6cd1bea098b9af47d563ea8963dabb65c8441a7e786912f1d1452825906f57c545a7e02ab46a4df'
 
@@ -8,59 +9,101 @@ const Markup = require('node-vk-bot-api/lib/markup')
 
 const bot = new VkBot(api_key);
 
-bot.command('/start', (ctx) => {
+bot.command(['/start', 'Начать', 'start', 'Start', 'начать', 'Старт', 'старт'], (ctx) => {
     ctx.reply('Если хочешь, я расскажу про вакансии.', null, Markup
         .keyboard([
             Markup.button({
                 action: {
-                    type: 'text', // Тип кнопки
-                    label: 'Хочу посмотреть открытые вакансии!', // Текст
+                    type: 'text',
+                    label: 'Хочу посмотреть открытые вакансии!',
                     payload: JSON.stringify({
-                        button: 'act1', // Полезная нагрузка на кнопку при нажатии её, вк будет передавать этот текст
+                        button: 'act1',
                     }),
                 },
-                color: 'default', // цвет текста
+                color: 'primary',
             }),
             Markup.button({
                 action: {
-                    type: 'text', // Тип кнопки
-                    label: 'Хочу поболтать с менеджером!', // Текст
+                    type: 'text',
+                    label: 'Хочу поболтать с менеджером!',
                     payload: JSON.stringify({
-                        button: 'act2', // Полезная нагрузка на кнопку при нажатии её, вк будет передавать этот текст
+                        button: 'act2',
                     }),
                 },
-                color: 'default', // цвет текста
+                color: 'primary',
             }),
         ], { columns: 1 }).oneTime());
 });
+
 
 bot.command('Хочу посмотреть открытые вакансии!', async (ctx) => {
     let obj = loadJsonFile.sync('./vacancies.json');
 
     let varArr = [];
 
-    for(let i = 0; i < obj.length; i++) {
-        if(obj[i].is_reserve == false)
-        {
+    let fullArr = [];
+
+    for (let i = 0; i < obj.length; i++) {
+
+        fullArr.push({
+            id: obj[i].id,
+            name: obj[i].name,
+            is_reserve: obj[i].is_reserve,
+            files: obj[i].files ? obj[i].files : null
+        })
+
+        if (obj[i].is_reserve == false) {
             varArr.push(
                 Markup.button({
                     action: {
                         type: 'text', // Тип кнопки
                         label: obj[i].name, // Текст
                         payload: JSON.stringify({
-                            button: 'wer', // Полезная нагрузка на кнопку при нажатии её, вк будет передавать этот текст
+                            button: 'act3',
                         }),
                     },
-                    color: 'default', // цвет текста
+                    color: 'primary', // цвет текста
                 })
             )
+
         }
         console.log(obj[i].name)
     }
 
+    varArr.push(
+        Markup.button({
+            action: {
+                type: 'text', // Тип кнопки
+                label: 'Я пас.', // Текст
+                payload: JSON.stringify({
+                    button: 'wer', // Полезная нагрузка на кнопку при нажатии её, вк будет передавать этот текст
+                }),
+            },
+            color: 'negative', // цвет текста
+        })
+    )
+
+    for (let i = 0; i < fullArr.length; i++) {
+        bot.command(fullArr[i].name.toString(), async ctx => {
+
+            try {
+                let buff = Buffer.from(fullArr[i].files[0].content.toString(), 'base64');
+                fs.writeFile(fullArr[i].files[0].file_name.toString(), buff, async function (error) {
+                    if (error) throw error; // если возникла ошибка
+                    let data = fs.readFileSync(fullArr[i].files[0].file_name.toString(), "utf8");
+                //    console.log(buff)
+                    await ctx.reply("Вот информация о вакансии.", fullArr[i].files[0].file_name.toString());
+                });
+
+            } catch (e) {
+             //   console.error(e);
+            }
+        });
+    }
+
     try {
-        await ctx.reply('testtest', null, Markup
-        .keyboard(varArr, { columns: 1 }).oneTime());
+        await ctx.reply('Класс! Вот наши вакансии, выбирай понравившуюся :)', null, Markup
+            .keyboard(varArr, { columns: 1 }).oneTime(true));
     } catch (e) {
         console.error(e);
     }
